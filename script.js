2b7dacd3a420d755eb64ef45ed0f3948a99906fc
Luis Miguel <luismiguelsilvaamorim@gmail.com>
2021-06-15 14:44:24 -0300

    Added :hover in css and JS file

---
+++ script.js
@@ -0,0 +1,4 @@
+const login = document.getElementsByClassName('buttonsdiv')
+const apps = document.getElementById('apps')
+const words = document.getElementsByClassName('words')
+const buttons = document.getElementsByClassName('buttons') //Submit buttons for search only
